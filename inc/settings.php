9088a2a2040d0be445802802add6e7422f775dbc
Pea Lutz <pea@glocal.coop>
2016-08-06 13:23:11 -0400

    Removed parent theme Theme Settings > Logo since it's duplicative of the native WP `custom-logo`, added in 4.5

--- inc/settings.php
+++ inc/settings.php
@@ -9,3 +9,16 @@ function cvar_remove_parent_admin_menus() {
     remove_submenu_page( 'themes.php', 'so_panels_home_page' );
 }
 add_action( 'admin_init', 'cvar_remove_parent_admin_menus', 999 );
+
+/**
+ * Remove parent theme settings
+ * Theme Settings > Logo is duplicative of the native WP `custom-logo`, added in 4.5
+ *
+ * @uses customize_register
+ * @uses remove_section
+ * @link https://codex.wordpress.org/Plugin_API/Action_Reference/customize_register
+ */
+function cvar_remove_parent_theme_settings( $wp_customize ) {
+    $wp_customize->remove_section( 'theme_settings_logo' );
+}
+add_action( 'customize_register', 'cvar_remove_parent_theme_settings', 20 );
