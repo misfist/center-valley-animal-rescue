be835cb686db665de7a739efd2fd6db462715cf9
Pea Lutz <pea@glocal.coop>
2017-05-07 15:31:33 -0400

    Child theme.

--- loops/loop-widget.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Loop Name: Widget Loop
- */
-?>
-
-<li id="post-<?php the_ID(); ?>" <?php post_class( 'post widget-item' ); ?>>
-	<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'cvar' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"  class="entry-title"><?php the_title(); ?></a>
-</li><!-- #post-<?php the_ID(); ?> -->
