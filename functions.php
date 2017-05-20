412af5f8b6c25a450052fc33429a03cb612e57e2
Pea <pea@misfist.com>
2017-05-19 22:57:23 -0400

    Added Petfinder API Library

--- functions.php
+++ functions.php
@@ -11,7 +11,10 @@ function pet_rescue_child_enqueue_styles() {
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
     wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
-}
 
+    if( !is_admin() ) {
+      wp_enqueue_script( 'petfinder', get_stylesheet_directory_uri() . '/assets/js/petfinder/dist/js/petfinderAPI4everybody.js', array( 'jquery' ), null, false );
+      wp_enqueue_script( 'petfinder-sort', get_stylesheet_directory_uri() . '/assets/js/petfinder/dist/js/petfinderSort.js', array( 'petfinder' ), null, false );
+    }
+}
 add_action( 'wp_enqueue_scripts', 'pet_rescue_child_enqueue_styles' );
-?>
