d2eb3a28b7c3e523f18c4f68b96179284361ca55
Pea <pea@misfist.com>
2017-08-24 15:50:49 -0400

    Initial Commit

--- functions.php
+++ functions.php
@@ -1,20 +1,12 @@
 <?php
-/**
- * @package 	WordPress
- * @subpackage 	Center_Valley_Animal_Rescue
- * @version		1.0.0
- */
 
-function pet_rescue_child_enqueue_styles() {
-    $parent_style = 'theme-style';
+add_action( 'wp_enqueue_scripts', 'karuna_parent_theme_enqueue_styles' );
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+function karuna_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'karuna-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'centervalley-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'karuna-style' )
+    );
 
-    wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
-
-    if( !is_admin() ) {
-      wp_enqueue_script( 'petfinder', get_stylesheet_directory_uri() . '/assets/js/petfinder/dist/js/petfinderAPI4everybody.js', array( 'jquery' ), null, false );
-      wp_enqueue_script( 'petfinder-sort', get_stylesheet_directory_uri() . '/assets/js/petfinder/dist/js/petfinderSort.js', array( 'petfinder' ), null, false );
-    }
 }
-add_action( 'wp_enqueue_scripts', 'pet_rescue_child_enqueue_styles' );
